d59938a84dcf6c3caa5c1691acf08733e32ab0c3
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-24 16:10:56 -0400

    Update esse.rb

--- Casks/esse.rb
+++ Casks/esse.rb
@@ -1,6 +1,7 @@
 cask "esse" do
     version "2020.5"
-    sha256 "d1aa57ce68003fffc7ba74abb0245ef9216038c0fa925467525c212cf2d5b736"
+    sha256 :no_check
+    
   
     url "https://github.com/amebalabs/Esse/releases/download/#{version}/Esse.zip"
     appcast "https://github.com/amebalabs/Esse/releases.atom"
