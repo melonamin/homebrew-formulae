a679c823894ac51cdd671cf799f4f0415e67b5be
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-24 16:04:11 -0400

    Update esse.rb

--- Casks/esse.rb
+++ Casks/esse.rb
@@ -2,7 +2,7 @@ cask "esse" do
     version "2020.5"
     sha256 "7beb29c883ebf74e3e8a6963613245ced1189067"
   
-    url "https://github.com/amebalabs/Esse/releases/download/v#{version}/Esse.zip"
+    url "https://github.com/amebalabs/Esse/releases/download/#{version}/Esse.zip"
     appcast "https://github.com/amebalabs/Esse/releases.atom"
     name "Esse"
     desc "Swiss army knife of text transformation for iOS and macOS"
