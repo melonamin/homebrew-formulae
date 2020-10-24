1202f35d57d97c85b420f4a51ad4aab797186ee3
Alex Mazanov <alexandr.mazanov@gmail.com>
2020-10-24 12:55:38 -0400

    Update swiftlambda.rb

--- Formula/swiftlambda.rb
+++ Formula/swiftlambda.rb
@@ -1,7 +1,7 @@
 class Swiftlambda < Formula
   desc "AWS Lambda Swift functions made easy"
   homepage "https://github.com/melonamin/swiftlambda"
-  url "https://github.com/melonamin/swiftlambda.git", :tag => "0.1.0", :revision => "3d77e89fd7ecdb5595637fcc1434e4482b686c79"
+  url "https://github.com/melonamin/swiftlambda.git", :tag => "0.1.0", :revision => "3dbebb789df2ec9d3e1d5fb73a3c5b7b44eef632"
     head "https://github.com/melonamin/swiftlambda.git", :shallow => false
   
   depends_on :xcode => ["12.0", :build]
