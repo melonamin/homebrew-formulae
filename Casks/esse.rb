0acb74a9563ebe7989eba87c3b9003d667a81f9f
Alexander Mazanov <alexandr.mazanov@gmail.com>
2020-10-24 16:03:11 -0400

    Create esse.rb

---
+++ Casks/esse.rb
@@ -0,0 +1,13 @@
+cask "shuttle" do
+    version "2020.5"
+    sha256 "7beb29c883ebf74e3e8a6963613245ced1189067"
+  
+    url "https://github.com/amebalabs/Esse/releases/download/v#{version}/Esse.zip"
+    appcast "https://github.com/amebalabs/Esse/releases.atom"
+    name "Esse"
+    desc "Swiss army knife of text transformation for iOS and macOS"
+    homepage "https://esse.ameba.co"
+  
+    app "Esse.app"
+  
+  end
