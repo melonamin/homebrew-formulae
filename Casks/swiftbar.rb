2847b41d09c316f6ae78f6b99cd529cdba4bea48
Alex Mazanov <alexandr.mazanov@gmail.com>
2020-12-16 22:12:15 -0500

    Update swiftbar.rb

--- Casks/swiftbar.rb
+++ Casks/swiftbar.rb
@@ -1,6 +1,6 @@
 cask "swiftbar" do
-    version "v1.1.0"
-    sha256 "c6adf11d0eaa1e1022154649f9bf9055161d8ffd2340abc90ed505321290b3b5"
+    version "v1.1.1"
+    sha256 "5431f9f1a3a7f423967dcbf2e7a4dfa10113c1f8531d325f04272466e61ea3ce"
   
     url "https://github.com/swiftbar/SwiftBar/releases/download/#{version}/SwiftBar.zip"
     appcast "https://github.com/swiftbar/SwiftBar/releases.atom"
