15ce8cf41ae137fb0dfbde25e014d613a739e5a3
Alex Mazanov <alexandr.mazanov@gmail.com>
2021-02-13 12:03:41 -0500

    Update swiftbar.rb

--- Casks/swiftbar.rb
+++ Casks/swiftbar.rb
@@ -2,11 +2,17 @@ cask "swiftbar" do
     version "v1.2.1"
     sha256 "5f662046811f932f9ce6340110bdebb7bfd6baaa795eb5ceb66dcf7e9a40f36c"
   
-    url "https://github.com/swiftbar/SwiftBar/releases/download/#{version}/SwiftBar.zip"
-    appcast "https://github.com/swiftbar/SwiftBar/releases.atom"
+    url "https://github.com/swiftbar/SwiftBar/releases/download/#{version}/SwiftBar.zip",
+      verified: "github.com/swiftbar/SwiftBar/releases/"
     name "SwiftBar"
     desc "Powerful macOS menu bar customization tool"
     homepage "https://swiftbar.app/"
   
+    livecheck do
+        url :url
+        strategy :github_latest
+    end
+
+    auto_updates true
     app "SwiftBar.app" 
 end
