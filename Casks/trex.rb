b9c7354528d2baef1d57ae932b3f4436d3b3a0a1
Alexander Mazanov <alexandr.mazanov@gmail.com>
2021-02-20 11:18:10 -0500

    Fixing typo

--- Casks/trex.rb
+++ Casks/trex.rb
@@ -5,7 +5,7 @@ cask "trex" do
     version "v1.0"
     sha256 "7a83e4e4c000591b2e549081500b74c4de42100552f31df685c41fd782f95a8a"
 
-    url " https://github.com/amebalabs/TRex/releases/download/#{version}/TRex.zip",
+    url "https://github.com/amebalabs/TRex/releases/download/#{version}/TRex.zip",
         verified: "github.com/amebalabs/TRex/releases/"
     name "TRex"
     desc "Easy to use text extraction tool for macOS"
