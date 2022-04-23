fc95769d0a29bb4e93cc165de3469822dbdda5d3
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-04-23 14:37:51 -0400

    Update ninefortyone.rb

--- Casks/ninefortyone.rb
+++ Casks/ninefortyone.rb
@@ -1,5 +1,5 @@
 cask "ninefortyone" do
-    version "1.0.0"
+    version "v1.0.0"
     sha256 :no_check
     
     url "https://github.com/amebalabs/NineFortyOne/releases/download/#{version}/NineFortyOne.zip"
