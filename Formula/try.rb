cf336e0876740f581a37513ed2c731c9fdde53ed
Alex Mazanov <alexandr.mazanov@gmail.com>
2025-08-21 20:40:45 -0400

    Fix version
    Signed-off-by: Alex Mazanov <alexandr.mazanov@gmail.com>

--- Formula/try.rb
+++ Formula/try.rb
@@ -1,7 +1,7 @@
 class Try < Formula
   desc "Fresh directories for every vibe - manage your experiment directories with style"
   homepage "https://github.com/melonamin/try"
-  url "https://github.com/melonamin/try/releases/download/v1.0.0/try-macos-universal.zip"
+  url "https://github.com/melonamin/try/releases/download/v0.1.0/try-macos-universal.zip"
   sha256 "1dafe0ac4bd1c2a0a9262d8974a8cbd882661be2ee86560adf106982dd11a6b3"
   license "MIT"
 
