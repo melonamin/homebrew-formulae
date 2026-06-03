f54c7f3441e68ee8bbbc8cd99e5348592a316df2
Alex Mabe <alex@melonamin.com>
2026-06-03 09:49:03 -0400

    gt 0.5.0

--- Formula/gt.rb
+++ Formula/gt.rb
@@ -1,8 +1,8 @@
 class Gt < Formula
   desc "Blazing fast TUI for managing Git worktrees with zero friction"
   homepage "https://github.com/melonamin/gt"
-  url "https://github.com/melonamin/gt/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "290fc7135cc4b3452e0564703b6141d5057bb5cc06d3ec0437dbf927e7550590"
+  url "https://github.com/melonamin/gt/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "cb92998de6c91ffb9bda974baea31dea4a99a3fb5f3a72e18b86f3ef1d8de64a"
   license "MIT"
   head "https://github.com/melonamin/gt.git", branch: "main"
 
